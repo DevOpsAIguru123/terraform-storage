dbf4eec10765edf41db2dbe2f9003775db917239
vinod vulavakayala <38119256+VinodVulavakayala@users.noreply.github.com>
2024-08-17 17:15:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,11 +19,16 @@ resource "azurerm_resource_group" "rg" {
   location = "your-location"
 }
 
-# Create an Azure Storage Account
-resource "azurerm_storage_account" "storage_account" {
+##  Demo now
+resource "azurerm_storage_account" "StorageAccountDemo" {
   name                     = "tfc-storage-account-dev"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    video = "azure"
+    channel = "CloudQuickLabs"
+  }
 }
