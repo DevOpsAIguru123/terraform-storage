5c5e100b7ce6db5768bbb604c1ea9b4fcfcc40a0
vinod vulavakayala <38119256+VinodVulavakayala@users.noreply.github.com>
2024-08-17 17:34:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,3 +32,14 @@ resource "azurerm_storage_account" "StorageAccountDemo" {
     channel = "CloudQuickLabs"
   }
 }
+
+resource "azurerm_databricks_workspace" "example" {
+  name                = "databricks-dev"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "standard"
+
+  tags = {
+    Environment = "dev"
+  }
+}
