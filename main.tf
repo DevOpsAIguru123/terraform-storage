6d5ae79c8954416d26def1d586200145652be0d4
vinod vulavakayala <38119256+VinodVulavakayala@users.noreply.github.com>
2024-08-17 17:17:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "rg" {
 
 ##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "tfc-storage-account-dev"
+  name                     = "tfc-dev"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
