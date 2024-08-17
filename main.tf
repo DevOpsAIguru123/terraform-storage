9f0957911c83291b39a476e035be84f021c0aefc
vinod vulavakayala <38119256+VinodVulavakayala@users.noreply.github.com>
2024-08-17 16:50:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,22 @@ terraform {
     } 
   } 
 }
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "tfc-storage-rg"
+  location = "your-location"
+}
+
+# Create an Azure Storage Account
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "tfc-storage-account-dev"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account tier             = "Standard"
+  account_replication_type = "LRS"
+}
