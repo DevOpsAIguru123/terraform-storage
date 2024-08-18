3d47b3e964a5a6f963a20adb1b39f73e622a9e0b
vinod vulavakayala <vinoddevopscloud99@gmail.com>
2024-08-17 20:04:55 -0500

    updated storage tf configs

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  allow_blob_public_access = false
+  #allow_blob_public_access = false
   min_tls_version          = "TLS1_2"
 }
 
