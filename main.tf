aebad3d7c80cb5ea88d60d56f162e9e77ad17d6e
vinod vulavakayala <vinoddevopscloud99@gmail.com>
2024-08-17 20:02:59 -0500

    updated storage resource rf

--- main.tf
+++ main.tf
@@ -97,12 +97,11 @@ output "cluster_id" {
 
 ### meta store ###
 resource "azurerm_storage_account" "example" {
-  name                     = metastore
+  name                     = "metastore"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
   allow_blob_public_access = false
   min_tls_version          = "TLS1_2"
 }
