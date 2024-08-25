ffc0dd3421de8fed89e4e4f48bafef6d862fd5eb
vinod vulavakayala <vinoddevopscloud99@gmail.com>
2024-08-25 15:43:11 -0500

    added databricks terraform file

--- main.tf
+++ main.tf
@@ -1,60 +1,43 @@
-terraform { 
-  cloud { 
-    
-    organization = "devops_vv" 
-
-    workspaces { 
-      name = "devops-dev" 
-    } 
-  }
-  
-   
-}
-
-
+# Azure Provider source and version being used
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
     databricks = {
       source  = "databricks/databricks"
-      version = "1.0.0"
+      version = ">=1.13.0"
     }
   }
 }
-provider "azurerm" {
-  features {}
+
+resource "random_string" "naming" {
+  special = false
+  upper   = false
+  length  = 6
 }
-variable "appname" {  
-  type = string
-  default = "myapp"
-  
-}
-variable "env" {
-  type = string
-  default = "dev"
-  
+
+resource "azurerm_resource_group" "this" {
+  name     = "${random_string.naming.result}-basic-demo-rg"
+  location = var.rglocation
+  tags     = local.tags
 }
 
 locals {
-  resource_group_name = "${var.appname}-rg-${var.env}"
+  prefix   = join("-", [var.workspace_prefix, "${random_string.naming.result}"])
+  location = var.rglocation
+  dbfsname = join("", [var.dbfs_prefix, "${random_string.naming.result}"]) // dbfs name must not have special chars
+
+  // tags that are propagated down to all resources
+  tags = merge({
+    Environment = "Testing"
+    Epoch       = random_string.naming.result
+  }, var.tags)
 }
 
-# Create a Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "${local.resource_group_name}"
-  location = "southcentralus"
-}
 
-# creat virtual network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.appname}-vnet-${var.env}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = ["10.0.0.0/16"]
-}
-# create subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.appname}-subnet-${var.env}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.0.0/24"]
+data "databricks_spark_version" "latest_lts" {
+  long_term_support = true
+  depends_on        = [azurerm_databricks_workspace.example]
 }
